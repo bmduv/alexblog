479022e01032298dee722ae044169282c58f9a4f
Alex Kim <alexjkim0717@gmail.com>
2024-12-06 22:39:03 -0800

    edit5

--- content/_index.md
+++ content/_index.md
@@ -1,8 +1,8 @@
 ---
-title: Welcome!👋
+title: " "
 tags:
 ---
-<img src="e30drift.PNG" padding=inherit display=block width=30% margin=auto text-align=center>
+<img src="e30drift.png" display=block height=100 margin=0 auto text-align=center>
 
-
+# Welcome!👋
 ### I'm using this to track my study notes + any blogs I feel like writing down, you can see my notes [here](/posts/).
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,12 +1,13 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-
+import { SimpleSlug } from "./quartz/util/path"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   footer: Component.Footer({
     links: {
+      Linkedin: "https://www.linkedin.com/in/alexjkim2000/"
     },
   }),
 }
@@ -24,11 +25,18 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.RecentNotes({
+        title: "Recent Writing",
+        limit: 4,
+        filter: (f) =>
+          f.slug!.startsWith("posts/") && f.slug! !== "posts/index" && !f.frontmatter?.noindex,
+        linkToMore: "posts/" as SimpleSlug,
+      }),
+    ),
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
